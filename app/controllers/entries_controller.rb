bdb24e43a9de512cda39f4fe3ab42c212135fa1a
whoisasey <atinachang@gmail.com>
2022-04-07 21:56:17 -0400

    add calculations and basic styling

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,28 @@
+require 'date'
+require "active_support"
+require "active_support/core_ext/date/calculations"
+
 class EntriesController < ApplicationController
   def index
     @entries = Entry.all
+    @start_date = Entry.last.created_at
+    @current = ((DateTime.new(2022,3,24))...(@start_date)).count
+    end_of_month = Date.today.at_end_of_month.strftime('%d').to_f
+
+    get_alc_free_days = @entries.select{ |entry|
+        entry.drinks != 0
+    }.count
+
+    last_entry = @start_date.strftime('%d').to_f
+    
+    # get percentage of sober days for month
+    @sober_days_in_month = (((last_entry - get_alc_free_days) / end_of_month) * 100).to_i
+    # take last entry date - days with drinks logged
+    # total / #days in month * 100
+    
+    # @sober_days_of_year = (((last_entry - get_alc_free_days) / (Date.today.end_of_year.strftime('%d').to_f)) * 100)
+    # binding.break
+    
+    # get percentage of sober days for year
   end
 end
