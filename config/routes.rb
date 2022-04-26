3964e206c9fd82685464466a507584c76f67748a
whoisasey <atinachang@gmail.com>
2022-04-25 20:48:31 -0400

    add entries summary to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :entries
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
 end
