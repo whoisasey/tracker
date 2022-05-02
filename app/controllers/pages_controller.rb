39b8c0e6ea70e268a10ef5d24173610f99e5ea21
whoisasey <atinachang@gmail.com>
2022-05-02 16:07:45 -0400

    add month filter and sober day data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,22 @@
 class PagesController < ApplicationController
   def home
-    @entries = Entry.all
+    @month_entries = Entry.this_month
     if !Entry.last.blank?
-    @start_date = Entry.last.created_at
-    @current = ((DateTime.new(2022,3,24))...(@start_date)).count
     end_of_month = Date.today.at_end_of_month.strftime('%d').to_f
+    
+    # get number of sober days for month
+    @get_month_alc_free_days = @month_entries.select{ |entry|
+      entry.drinks == 0
+    }.count
+    
+    @sober_days_in_month = (( @get_month_alc_free_days / end_of_month) * 100).to_i
 
-    get_alc_free_days = @entries.select{ |entry|
-        entry.drinks != 0
+    # get sober days this year
+    @sober_days_of_year = Entry.all.select { |entry|
+      entry.drinks == 0
     }.count
 
-    last_entry = @start_date.strftime('%d').to_f
-    
-    # get percentage of sober days for month
-    @sober_days_in_month = (((last_entry - get_alc_free_days) / end_of_month) * 100).to_i
-    # take last entry date - days with drinks logged
-    # total / #days in month * 100
-    
-    # @sober_days_of_year = (((last_entry - get_alc_free_days) / (Date.today.end_of_year.strftime('%d').to_f)) * 100)
-    # binding.break
-    
-    # get percentage of sober days for year
   else return nil
   end
-  end
+end
 end
