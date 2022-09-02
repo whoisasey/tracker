5c2bf90f5f842360f556031ebacfa4d1136d21f8
whoisasey <atinachang@gmail.com>
2022-09-02 14:26:58 -0400

    fix redirect on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Logged In Successfully"
-      redirect_to root_path
+      redirect_to new_entry_path
     else
       flash.now[:alert] = "There was something wrong with your login details"
       render 'new'
