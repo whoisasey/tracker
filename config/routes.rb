a2dc5a95e457eb011dfc96af006ab4b6d3358ed6
whoisasey <atinachang@gmail.com>
2022-09-02 14:27:30 -0400

    re-add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :entries
   get 'signup', to: 'users#new'
-  # POST signup?
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
