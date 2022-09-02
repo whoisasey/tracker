387f74b1288cf921710b9b872f8abba106369b2c
whoisasey <atinachang@gmail.com>
2022-09-02 14:04:08 -0400

    add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :entries
+  get 'signup', to: 'users#new'
+  # POST signup?
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
