e0b2ac6a3c00667410161ff6842b291daf4d408c
whoisasey <atinachang@gmail.com>
2022-09-02 14:03:41 -0400

    add new user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params.require(:user).permit(:username, :password))
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "#{@user.username}, You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
