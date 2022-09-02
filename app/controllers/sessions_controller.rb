e999bb7dd141bf44247b6b714d1bedfd14067b28
whoisasey <atinachang@gmail.com>
2022-09-02 14:03:19 -0400

    add sessions for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged In Successfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new'
+    end
   end
 
   def login
